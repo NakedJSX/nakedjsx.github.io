ce19d02e76f29beb7dc10f643be3d804d87fbe84
dqh-au <david.q.hogan@gmail.com>
2023-05-16 22:02:34 +1000

    'Coming soon' website placeholder

---
+++ src/index-html.mjs
@@ -0,0 +1,27 @@
+import { Page } from '@nakedjsx/core/page'
+
+import logo from ':raw:$ASSET/logo.svg'
+
+const Head =
+    () =>
+    <>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>NakedJSX</title>
+    </>
+
+const Body =
+    () =>
+    <>
+        <main>
+            <h1 css="
+                    border-bottom: 1px solid var(--fg-color);
+                    & svg { display: inline }
+                "><raw-content content={logo} /> Coming Soon.</h1>
+        </main>
+    </>
+
+Page.Create('en');
+Page.AppendHead(<Head/>);
+Page.AppendBody(<Body/>);
+Page.Render();
